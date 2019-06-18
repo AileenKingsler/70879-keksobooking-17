cf05234f99f6d0404981c637e525f0dec5dc12dc
Aileen <aileen.kingsler@mail.ru>
2019-06-18 17:59:05 +1000

    Добавляет валидацию формы

--- js/main.js
+++ js/main.js
@@ -99,3 +99,39 @@ mainPin.addEventListener('click', function () {
   toggleDisableAttr(adFormElements, false);
   toggleDisableAttr(mapFiltersElements, false);
 });
+
+
+// Валидация формы
+
+var typeField = adForm.querySelector('#type');
+var priceField = adForm.querySelector('#price');
+
+typeField.addEventListener('change', function () {
+  if (typeField.value === 'bungalo') {
+    priceField.min = priceField.placeholder = 0;
+  }
+  if (typeField.value === 'flat') {
+    priceField.min = priceField.placeholder = 1000;
+  }
+  if (typeField.value === 'house') {
+    priceField.min = priceField.placeholder = 5000;
+  }
+  if (typeField.value === 'palace') {
+    priceField.min = priceField.placeholder = 10000;
+  }
+});
+
+var timeInField = adForm.querySelector('#timein');
+var timeOutField = adForm.querySelector('#timeout');
+
+timeInField.addEventListener('change', function () {
+  if (timeInField.value !== timeOutField.value) {
+    timeOutField.value = timeInField.value;
+  }
+});
+
+timeOutField.addEventListener('change', function () {
+  if (timeInField.value !== timeOutField.value) {
+    timeInField.value = timeOutField.value;
+  }
+});
